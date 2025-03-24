6b915615077255e3e48be0e9209fb0536d5a76da
bezerraescossia <44624917+bezerraescossia@users.noreply.github.com>
2025-03-24 15:29:59 +0000

    Deploying to gh-pages from @ bezerraescossia/bezerraescossia.github.io@bfcdf1e0b12c346896524b33ae97129932449645 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -56,23 +56,7 @@ ninja.data = [{
           window.open("https://medium.com/@bezerraescossia/the-power-of-spatial-statistics-in-business-missed-opportunities-and-strategic-potential-db0246060847", "_blank");
         
       },
-    },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{
+    },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
